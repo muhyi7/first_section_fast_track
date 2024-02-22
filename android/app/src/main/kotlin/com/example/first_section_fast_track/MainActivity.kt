5fba62e1c2e103fe556925b1024a18391bc52be8
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-02-22 18:09:21 +0700

    Section 1

---
+++ android/app/src/main/kotlin/com/example/first_section_fast_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_section_fast_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+void main(){
+  var nama = "JONO";
+  var umur = 90;
+
+  // var umur = "20";
+
+
+  // var data = [1,2,3,4];
+  // var kelompok =["PANJUL","YANTO","SUMANTO"];
+  
+  print("NAMA SAYA : $nama & UMUR SAYA : $umur TAHUN");
+  print(nama);
+  print(umur);
+}
